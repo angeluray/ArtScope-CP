8ca07023dddcdf1542248c7db24c27e8848b983a
angeluray <aeucampos@gmail.com>
2022-07-28 03:14:24 -0400

    Add Home Desktop Version and Styles

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 const menuHamburguer = document.querySelector('.burger-menu');
 const myNav = document.querySelector('.organizer');
+const myBody = document.querySelector('body');
 
 menuHamburguer.addEventListener('click', () => {
   menuHamburguer.classList.toggle('active');
   myNav.classList.toggle('active');
+  myBody.classList.toggle('active');
 });
 
 document.querySelectorAll('.link-nav').forEach((n) => n.addEventListener('click', () => {
